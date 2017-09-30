e76ce2a2c5cccd5b49cfcad5c5387dccb5d4bdb2
Bruno Caldas <brunopaivacaldas@hotmail.com>
2017-09-30 01:50:26 -0300

    Criação do DAO de Remédios e mudança to tema

---
+++ app/src/main/java/br/com/brunoedalcilene/horadoremdio/dao/RemedioDao.java
@@ -0,0 +1,85 @@
+package br.com.brunoedalcilene.horadoremdio.dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import java.util.ArrayList;
+import java.util.List;
+import br.com.brunoedalcilene.horadoremdio.database.Database;
+import br.com.brunoedalcilene.horadoremdio.model.ETipo;
+import br.com.brunoedalcilene.horadoremdio.model.Remedio;
+
+/**
+ * Created by bruno on 30/09/2017.
+ */
+
+public class RemedioDao {
+
+    Context context;
+    Database banco;
+    SQLiteDatabase c;
+
+    public RemedioDao(Context context) {
+        this.context = context;
+        banco = new Database(context);
+    }
+
+    private void open(){
+        c = banco.getWritableDatabase();
+    }
+    
+    private void close(){
+        c.close();
+        banco.close();
+    }
+
+    public void inserir(Remedio remedio){
+        open();
+        try {
+            ContentValues cv = new ContentValues();
+            cv.put(Database.REMEDIO_NOME, remedio.getNome());
+            cv.put(Database.REMEDIO_DESC, remedio.getDescricao());
+            cv.put(Database.REMEDIO_TIPO, remedio.getTipo().toString());
+            cv.put(Database.REMEDIO_UNIDADE, remedio.getMiligramas());
+
+            c.insert(Database.TABELA_REMEDIO,null,cv);
+        }finally {
+            close();
+        }
+    }
+    public List<Remedio> obterTodos(){
+        open();
+        try {
+            String sql = "select * from "+Database.TABELA_REMEDIO;
+            Cursor cur = c.rawQuery(sql,null);
+
+            List<Remedio> remedios = new ArrayList<>();
+
+            while(cur.moveToNext()){
+                Remedio r = new Remedio();
+                r.setId( cur.getInt( cur.getColumnIndex(Database.REMEDIO_ID ) ) );
+                r.setNome( cur.getString( cur.getColumnIndex(Database.REMEDIO_NOME ) ) );
+                r.setDescricao( cur.getString( cur.getColumnIndex(Database.REMEDIO_DESC ) ) );
+                r.setMiligramas( cur.getInt( cur.getColumnIndex(Database.REMEDIO_UNIDADE ) ) );
+                r.setTipo(
+                        ETipo.valueOf(cur.getString(
+                                cur.getColumnIndex(Database.REMEDIO_TIPO ) ) ) );
+                remedios.add(r);
+            }
+            return remedios;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            close();
+        }
+    }
+    public List<Remedio> obterPorNome(String nome){
+        return null;
+    }
+    public Remedio obterPorId(int id){
+        return null;
+    }
+}
